5afdaab62b9a74a4fbf3465d84b6a7e0fa30eeb4
Samuel Paluba <63147933+SamuelPalubaCZ@users.noreply.github.com>
2025-03-26 22:14:48 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
-# Install dependencies for Remarkable kernel compile script
-
-# List of dependencies
-dependencies=${dependencies:="git wget aria2"}
-
-# Remarkable toolchain URL 
-toolchain_url=${toolchain_url:="https://ipfs.eeems.website/ipfs/Qmdkdeh3bodwDLM9YvPrMoAi6dFYDDCodAnHvjG5voZxiC"}
-
-# Remarkable kernel source URL
-kernel_url=${kernel_url:="https://codeload.github.com/reMarkable/linux/zip/refs/heads/lars/zero-gravitas_4.9"}
+# Download function for Remarkable kernel compile script
 
 download () {
 downloader=${downloader:="aria2"}
